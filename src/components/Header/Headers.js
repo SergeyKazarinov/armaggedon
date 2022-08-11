d251b2f6f36751c7781fd5c009fd5b5f09e50b6b
Sergey Kazarinov <kazarinov092@gmail.com>
2022-08-11 16:22:35 +0500

    feat(header): add adaptiv for 320px

--- src/components/Header/Headers.js
+++ src/components/Header/Headers.js
@@ -6,15 +6,15 @@ import headerImage from '../../images/headerBackground.jpg'
 function Header() {
   return(
     <header className={header.header}>
-      <div className={app.flex}>
+      <div className={header.flex_container}>
         <div className={`${app.flex} ${app.flex_column}`}>
           <h1 className={header.title}>ARMAGGEDON V2</h1>
           <p className={header.subtitle}>Сервис заказа уничтожения астероидов, опасно подлетающих к Земле.</p>
         </div>
         <nav className={app.flex}>
           <ul className={`${app.flex} ${header.nav}`}>
-            <li className={header.item}>Астероиды</li>
-            <li className={header.item}>Заказ</li>
+            <li className={`${header.item}`}>Астероиды</li>
+            <li className={`${header.item}`}>Заказ</li>
           </ul>
         </nav>
       </div>
