d0b727e8c986aae00d68f4b6f86dc37c5f80539d
Sergey Kazarinov <kazarinov092@gmail.com>
2022-08-12 18:09:24 +0500

    feat(scroll): add method get asteroidsData from handleScroll

--- src/App.js
+++ src/App.js
@@ -7,13 +7,11 @@ import api from './utils/Api';
 
 function App() {
   const date = new Date();
+  const [currentPage, setCurrentPage] = useState(1)
   const [headerImage, setHeaderImage] = useState('');
-  const [res, setResult] = useState({});
   const [asteroids, setAsteroids] = useState([]);
   const [isAsteroidPage, setIsAsteroidPage] = useState(true);
-  // console.log(date.toLocaleString());
-  // date.setDate(date.getDate() + 1)
-  // console.log(`новая дата ${date.toLocaleString()}`);
+  const [fetching, setFetching] = useState(false);
 
   useEffect(() => {
     api.getApodImage()
@@ -35,7 +33,42 @@ function App() {
       .catch((err) => {
         console.log(err);
       });
+
+      document.addEventListener('scroll', handleScroll);
+
+      return function() {
+        document.removeEventListener('scroll', handleScroll);
+      }
   }, [])
+
+  useEffect(() => {
+    if(fetching) {
+      date.setDate(date.getDate() + currentPage)
+      api.getInitialAsteroids(date)
+      .then((res) => {
+        let arr = [];
+        for(let key in res.near_earth_objects) {
+          arr = asteroids.concat(res.near_earth_objects[key]);
+        }
+        setAsteroids(arr)
+        setCurrentPage(preState => preState + 1);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setFetching(false);
+      });
+    }
+
+  }, [fetching])
+
+  function handleScroll(e) {
+    if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100) {
+      setFetching(true);
+    }
+    
+  }
 
   function handleHeaderButtonClick(boolean) {
     setIsAsteroidPage(boolean)
